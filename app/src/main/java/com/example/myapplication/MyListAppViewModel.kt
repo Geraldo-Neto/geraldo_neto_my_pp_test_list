255bd55ff7d828fe1a3874e7306d62f11e1b815d
Geraldo <geraldoneto-123@hotmail.com>
2025-03-25 17:08:29 -0300

    added viewmodel comment

--- app/src/main/java/com/example/myapplication/MyListAppViewModel.kt
+++ app/src/main/java/com/example/myapplication/MyListAppViewModel.kt
@@ -2,7 +2,7 @@ package com.example.myapplication
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-
+//didnt use, because of time, but would have this as mvvm, using states and events
 class MyListAppViewModel: ViewModel() {
     private val _state = MutableStateFlow<State>(State())
 
